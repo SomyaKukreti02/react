831b09ca7aebc6d350178cff9bb9f1e65e6d5b8a
somyakukreti02 <kukretisomya02@gmail.com>
2024-11-12 16:33:21 +0530

    with the completion of 03 we are done with the basics of react

---
+++ 03tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+import SignIn from "./components/SignIn.jsx";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1>Sign-In-Section</h1>
+        <SignIn />
+      </section>
+    </>
+  );
+}
+
+export default App;
