46e39087f69fc2d8e84119c1184db4b45017aca5
somyakukreti02 <kukretisomya02@gmail.com>
2024-11-17 00:30:09 +0530

    04bgchanger completed

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,12 +10,16 @@ function App() {
 
   const addValue = () => {
     if (counter < 20) {
+      // counter++;
+      // console.log(counter);
       setCounter(counter + 1);
     }
   };
 
   const removeValue = () => {
     if (counter > 0) {
+      // counter--;
+      // console.log(counter);
       setCounter(counter - 1);
     }
   };
@@ -24,9 +28,9 @@ function App() {
     <>
       <h1>Chai aur react</h1>
       <h2>Counter value: {counter}</h2>
-      <button onClick={addValue}>Add value{counter}</button>
+      <button onClick={addValue}>Add value {counter}</button>
       <br />
-      <button onClick={removeValue}>remove value{counter}</button>
+      <button onClick={removeValue}>remove value {counter}</button>
       <p>footer:{counter}</p>
     </>
   );
